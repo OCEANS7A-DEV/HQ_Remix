e617093abdb4624b5e2ecfd8c237f9949e696ef1
OCEANS7A-DEV <oceans7a@gmail.com>
2025-03-19 14:11:19 +0900

    ts

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,14 @@
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-  useNavigation,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
-import { useEffect, useState } from "react";
+
 import "./tailwind.css";
-import { LinearProgress } from '@mui/material';
+
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
@@ -25,19 +23,6 @@ export const links: LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
-
-  const navigation = useNavigation();
-  const [loading, setLoading] = useState(navigation.state === 'loading');
-
-
-  useEffect(() => {
-    console.log(navigation.state)
-    if (navigation.state === "loading") {
-      setLoading(true);
-    } else {
-      setLoading(false);
-    }
-  }, [navigation.state]);
   return (
     <html lang="ja">
       <head>
@@ -47,23 +32,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {loading && (
-          <div
-            style={{
-              position: "fixed",
-              top: 0,
-              left: 0,
-              width: "100%",
-              zIndex: 100,
-            }}
-          >
-            <LinearProgress color="primary" style={{ width: '100%' }} value={50}/>
-          </div>
-        )}
         {children}
         <ScrollRestoration />
         <Scripts />
-        <LiveReload/>
       </body>
     </html>
   );
